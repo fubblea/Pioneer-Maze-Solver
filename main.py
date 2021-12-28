6b41997259fbed2204e98b5a99fd629031cc9e7a
Ajay Anand <ajay.arthur@gmail.com>
2021-12-28 18:59:27 -0500

    Setup promity detection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sim
 import sys
+import numpy as np
 
 sim.simxFinish(-1)
 clientID = sim.simxStart('127.0.0.1', 19999, True, True, 5000, 5)
@@ -11,28 +12,49 @@ else:
 
 sim.simxStartSimulation(clientID=clientID, operationMode=sim.simx_opmode_oneshot)
 
-errorCode, motorLeft = sim.simxGetObjectHandle(
+returnCode, leftMotor = sim.simxGetObjectHandle(
                             clientID=clientID, 
                             objectName='Pioneer_p3dx_leftMotor', 
                             operationMode=sim.simx_opmode_oneshot_wait
                         )
 
-errorCode, motorRight = sim.simxGetObjectHandle(
+returnCode, rightMotor = sim.simxGetObjectHandle(
                             clientID=clientID, 
                             objectName='Pioneer_p3dx_rightMotor', 
                             operationMode=sim.simx_opmode_oneshot_wait
                         )
 
-errorCode = sim.simxSetJointTargetVelocity(
+returnCode, ultrasonicSensor12 = sim.simxGetObjectHandle(
+                                    clientID=clientID, 
+                                    objectName='Pioneer_p3dx_ultrasonicSensor12', 
+                                    operationMode=sim.simx_opmode_oneshot_wait
+                                )
+
+returnCode = sim.simxSetJointTargetVelocity(
                 clientID=clientID,
-                jointHandle=motorLeft,
-                targetVelocity=0.2,
+                jointHandle=leftMotor,
+                targetVelocity=0.3,
                 operationMode=sim.simx_opmode_oneshot
             )
 
-errorCode = sim.simxSetJointTargetVelocity(
+returnCode = sim.simxSetJointTargetVelocity(
                 clientID=clientID,
-                jointHandle=motorRight,
-                targetVelocity=0.4,
+                jointHandle=rightMotor,
+                targetVelocity=0.3,
                 operationMode=sim.simx_opmode_oneshot_wait
             )
+
+returnCode, detectionState, detectedPoint, detectedObjectHandle, detectedSurfaceNormalVector=sim.simxReadProximitySensor(
+    clientID=clientID,
+    sensorHandle=ultrasonicSensor12,
+    operationMode=sim.simx_opmode_streaming
+    )
+
+while True:
+    returnCode, detectionState, detectedPoint, detectedObjectHandle, detectedSurfaceNormalVector=sim.simxReadProximitySensor(
+    clientID=clientID,
+    sensorHandle=ultrasonicSensor12,
+    operationMode=sim.simx_opmode_buffer
+    )
+
+    print(np.linalg.norm(detectedPoint))
