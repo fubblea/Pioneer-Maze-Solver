2f3af96cdc29f78eda8604dd228116f28b438379
Ajay Anand <ajay.arthur@gmail.com>
2021-12-29 03:26:57 -0500

    Removed stopwatch implementation

--- wall_follower.py
+++ wall_follower.py
@@ -1,4 +1,3 @@
-import time
 import sys
 
 import sim
@@ -66,16 +65,6 @@ returnCode, collisionState = sim.simxCheckCollision(clientID, entity1=robot, ent
 NODETECTIONDIST = 0.15
 V0 = 1
 
-def time_convert(sec):
-    mins = sec // 60
-    sec = sec % 60
-    hours = mins // 60
-    mins = mins % 60
-    print("Time Taken = {0}:{1}:{2}".format(int(hours),int(mins),round(sec, 2)))
-
-print("Stopwatch Started")
-start_time = time.time()
-
 try:
     while collisionState == False:
         #Update Sonar Array
@@ -119,8 +108,3 @@ try:
 
 except KeyboardInterrupt:
     sim.simxStopSimulation(clientID, operationMode=sim.simx_opmode_oneshot_wait)
-
-print("Stopwatch Stopped")
-end_time = time.time()
-time_lapsed = end_time - start_time
-time_convert(time_lapsed)
